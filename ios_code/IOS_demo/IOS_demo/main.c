ff5d00a517665920d8c4ff8c3d244c2e85be9807
1143238608 <1143238608@qq.com>
2024-09-01 17:42:20 +0800

    提交

---
+++ ios_code/IOS_demo/IOS_demo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  IOS_demo
+//
+//  Created by mac on 2024/9/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, 太原理工大学\n");
+    return 0;
+}
